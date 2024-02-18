9fcc22449bcb8d5e06471f4b7e31bc07cb0fbf33
nithilprasathk <nithilprasath95@gmail.com>
2024-02-18 21:12:41 +0530

    homepage

--- src/homepage/MyCard.jsx
+++ src/homepage/MyCard.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { Card, Button } from "antd";
+import { Card } from "antd";
 const { Meta } = Card;
-const MyCard = ({ myImage, myDescription, myTitle, buttonText }) => (
+const MyCard = ({ myImage, myTitle }) => (
   <Card
     hoverable
     style={{
       width: 240,
+      height: 200,
+      borderRadius: "50%",
+      overflow: "hidden",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
     }}
     cover={<img alt="example" src={myImage} />}
   >
-    <Meta title={myTitle} description={myDescription} />
-    <Button type="primary" style={{ marginTop: "10px", marginLeft: "70%" }}>
-      {buttonText}
-    </Button>
+    <Meta
+      title={myTitle}
+      style={{ fontSize: "16px", fontWeight: "bold", marginTop: "-20px" }}
+    />
   </Card>
 );
 export default MyCard;
